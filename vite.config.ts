c29a9a0bbc3e23ea96878e78771067e27a8d3137
Agusx1211 <agusgit@pm.me>
2024-04-05 12:32:20 +0000

    Fix gh pages base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }: { mode: string }) => ({
   plugins: [react()],
-})
+  base: mode === 'production' ? '/enchanter/' : '/',
+}))
